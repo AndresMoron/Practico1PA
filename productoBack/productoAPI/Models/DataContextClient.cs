a3bc2f67b43dfe0fb5667144967a2cb9f1050178
Your Name <you@example.com>
2019-04-24 17:48:54 -0400

    Commit con modelo Web Api Clientes

---
+++ productoBack/productoAPI/Models/DataContextClient.cs
@@ -0,0 +1,16 @@
+﻿using System.Data.Entity;
+
+namespace productoAPI.Models
+{
+  
+
+    public class DataContextClient:DbContext
+    {
+        public DataContextClient():base("DefaultConnection")
+        {
+
+        }
+
+        public System.Data.Entity.DbSet<productoAPI.Client> Clients { get; set; }
+    }
+}
