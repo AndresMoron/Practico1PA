87f269594783b414abd2640ed563a658dabf1065
Your Name <you@example.com>
2019-04-24 17:17:34 -0400

    Commit con modelo MVC Clientes

---
+++ productoBack/productoBack/Models/DataContextClient.cs
@@ -0,0 +1,15 @@
+﻿
+namespace productoBack.Models
+{
+    using System.Data.Entity;
+
+    public class DataContextClient:DbContext
+    {
+        public DataContextClient():base("DefaultConnection")
+        {
+
+        }
+
+        public System.Data.Entity.DbSet<productoBack.Client> Clients { get; set; }
+    }
+}
